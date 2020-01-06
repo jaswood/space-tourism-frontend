8537bc60228f91402c27939ace6770e3ffe60f8a
Joseph Wood <jaswoodfb@gmail.com>
2020-01-06 17:28:37 +0000

    deleted unused componenet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MessagesComponent } from './messages/messages.component';
 import { MatCardModule } from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import { SelectFlightResolverService } from './select-flight/resolvers/select-flight-resolver';
@@ -52,7 +51,6 @@ const routes: Routes = [
   declarations: [
     AppComponent,
     SelectFlightComponent,
-    MessagesComponent,
     FlightDetailsComponent,
     SeatSelectionComponent,
     SpaceFlightPipe,
